df22f998ba77e43ceb646091fa85f83baf3cf223
restom0 <98004370+restom0@users.noreply.github.com>
2023-04-24 22:37:33 +0700

    Update Authenticate.java

--- src/main/java/com/hotelmanager/data/Authenticate.java
+++ src/main/java/com/hotelmanager/data/Authenticate.java
@@ -1,15 +1,15 @@
 package com.hotelmanager.data;
 
 public class Authenticate {
-    private String username;
-    private String password;
+    private String username = "admin";
+    private String password = "admin";
 
     public void changePassword(String password) {
         this.password = password;
     }
 
     public boolean checkLogin(String username, String password) {
-        if (this.username == username && this.password == password) {
+        if (this.username.equals(username) && this.password.equals(password)) {
             return true;
         }
         return false;
